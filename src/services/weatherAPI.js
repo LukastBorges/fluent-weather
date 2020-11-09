e2e57f4a69c21759c2be9dfa4b4c4ecc4c3e8561
Lucas Borges <lukasborges@outlook.com>
2020-11-09 15:55:32 -0500

    [006]: removes hard coded API key

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
-const API_KEY = '78df589f3d2e3293fde045e8099bd402'
+const API_KEY = '' // Replace by a valid API KEY from OpenWeather
 
 export const getByCoordinates = async (lat, lon) => {
   try {
